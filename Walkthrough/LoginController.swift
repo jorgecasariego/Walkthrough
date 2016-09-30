5499cd86d2b23dc106db9bd1baca061c303ad864
Jorge Casariego <jorgekasa@gmail.com>
2016-09-30 13:17:39 -0400

    Adding a new Navigation Controller when user is Logged In

--- Walkthrough/LoginController.swift
+++ Walkthrough/LoginController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class LoginController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -130,8 +130,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func keyboardShow() {
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
-            self.view.frame = CGRect(x: 0, y: -50, width: self.view.frame.width, height: self.view.frame.height)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            let y: CGFloat = UIDevice.current.orientation.isLandscape ? -100 : -50
+            self.view.frame = CGRect(x: 0, y: y, width: self.view.frame.width, height: self.view.frame.height)
             }, completion: nil)
     }
     
@@ -177,6 +178,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return pages.count + 1
     }
     
+    // Every time we call reloadData this method is called
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if indexPath.item == pages.count {
@@ -195,6 +197,22 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    // With this method we can know the orientation of the device
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        // With this line we redraw the view with the proper width and height for all the cells
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+        let indexPath = IndexPath(item: pageControl.currentPage, section: 0)
+        
+        // scroll to indexPath after the rotation  is going
+        DispatchQueue.main.async {
+            self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            self.collectionView.reloadData()
+        }
+        
     }
     
 }
---
+++ Walkthrough/MainNavigationController.swift
@@ -0,0 +1,51 @@
+//
+//  MainNavigationController.swift
+//  Walkthrough
+//
+//  Created by Jorge Casariego on 30/9/16.
+//  Copyright © 2016 Jorge Casariego. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        if isLoggedIn() {
+            // asume user is lodgged in
+            let homeController = HomeController()
+            //viewControllers is a property that exist in UINavigationController and it is an array of all the controllers that exists in the navigation stack
+            viewControllers = [homeController]
+        } else {
+            perform(#selector(showLoginController), with: nil, afterDelay: 0.01)
+        }
+    }
+    
+    fileprivate func isLoggedIn() -> Bool {
+        return true
+    }
+    
+    func showLoginController() {
+        let loginController = LoginController()
+        present(loginController, animated: true) { 
+            //Perhaps we'll do something here later
+        }
+    }
+}
+
+class HomeController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "You're logged In"
+        
+        let imageView = UIImageView(image: UIImage(named: "home"))
+        view.addSubview(imageView)
+        
+        _ = imageView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+    }
+}
